fa8c12d554fce0c74b081815adb25260b8d644ea
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-01-22 23:04:52 -0500

    Add instructor keyboard shortcuts and debug logs

--- static/js/instructor.js
+++ static/js/instructor.js
@@ -8,26 +8,61 @@ function nextPage() {
   socket.emit('instruction', {
     command: "next-page",
   });
+  logCommand("next-page");
 }
 
 function prevPage() {
   socket.emit('instruction', {
     command: "prev-page",
   });
+  logCommand("prev-page");
 }
 
 function upScale() {
   socket.emit('instruction', {
     command: "up-scale",
   });
+  logCommand("up-scale");
 }
 
 function downScale() {
   socket.emit('instruction', {
     command: "down-scale",
   });
+  logCommand("down-scale");
+}
+
+function logCommand(command) {
+  var dt = new Date();
+  var utcDate = dt.toUTCString();
+  $("#command-log").text("You pressed " + command + " at " + utcDate);
 }
 
 $(document).ready(function(){
   console.log("Connected as instructor...");
+
+  var body = $("body");
+  body.keydown(function(event){
+
+    if (event.which == "221") // "]"
+    {
+      nextPage();
+    }
+
+    if (event.which == "219") // "["
+    {
+      prevPage();
+    }
+
+    if (event.which == "187") // "+"
+    {
+      upScale();
+    }
+
+    if (event.which == "189") // "-"
+    {
+      downScale();
+    }
+
+  })
 })
