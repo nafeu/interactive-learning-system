b58b0dcde836ab7c74fc558b7d8febe319541b7c
Nafeu Nasir <nafeu.nasir@gmail.com>
2018-01-23 09:27:04 -0500

    Add attendee multiple choice response buttons

--- static/js/attendee.js
+++ static/js/attendee.js
@@ -1,10 +1,35 @@
 var socket = io({'reconnection': false});
 var url = '../assets/example.pdf';
+var attendeeButtonContainer,
+    attendeeSubmission;
 
 socket.on('new connection', function(data){
   console.log("connected with id: " + data.id);
 })
 
+socket.on('new-question', function(){
+  resetMultipleChoiceResponses();
+})
+
 $(document).ready(function(){
   console.log("Connected as attendee...");
+
+  attendeeButtonContainer = $("#attendee-button-container");
+  attendeeSubmission = $("#attendee-submission");
+
 })
+
+function sendMultipleChoiceResponse(choice) {
+  attendeeButtonContainer.hide();
+  socket.emit("attendee-response", choice);
+  attendeeSubmission.text("You chose " + choice);
+  attendeeSubmission.show();
+}
+
+function resetMultipleChoiceResponses() {
+  attendeeButtonContainer.show();
+}
+
+function submitComment(comment) {
+  socket.emit('attendee-comment', comment);
+}
